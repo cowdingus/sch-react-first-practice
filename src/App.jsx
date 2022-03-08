1fee710f5149b985a582a9fb830b6aa1a84afdfa
cowdingus <zaki.nadhif45@gmail.com>
2022-03-08 13:43:06 +0700

    Completed 'Menambahkan Bootstrap pada project React JS' chapter

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,17 @@
 import Alert from "./components/Alert";
 import Media from "./components/Media";
 import "./App.css";
-import {useState} from "react";
+import { useState } from "react";
 
 function App() {
   return (
-    <div className="App container col-sm-6">
-      <Alert type="danger" header="Fatal Error">
-        Ini adalah alert dengan type danger
-      </Alert>
-      <Alert type="success" header="Berhasil">
-        Ini adalah alert dengan type success
-      </Alert>
-      <Alert type="warning" header="Warning">
-        Ini adalah alert dengan type warning
-      </Alert>
-      <Media image="react.png" title="React JS">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem rerum magni
-        dolore aperiam, veniam aspernatur porro qui, ea iure ut saepe labore
-        commodi cupiditate minima illo voluptatem cum debitis quos.
-      </Media>
+    <div className="App container">
+      <div className="alert alert-info">
+        <h3 className="text-danger">Ini Project Pertama React JS</h3>
+        <p>Belajar React JS itu mudah</p>
+        <button className="mr-1 btn btn-success">Setuju</button>
+        <button className="btn btn-info">Iya Dong</button>
+      </div>
     </div>
   );
 }
